e6963114bbb568a982233dddee54219d8d2733c4
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-04-22 17:08:26 -0400

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>
-    <h1>Stayed tuned! The Website will soon be built :D</h1>
-  </div>
+    <v-app id="inspire">
+        <v-app-bar app color="white">
+            <v-tabs color="purple">
+                <v-tab>Item One</v-tab>
+                <v-tab>Item Two</v-tab>
+                <v-tab>Item Three</v-tab>
+            </v-tabs>
+        </v-app-bar>
+
+        <v-content>
+            <v-parallax
+                src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+                height="400"
+            >
+            </v-parallax>
+            <v-parallax
+                src="https://miro.medium.com/max/3622/1*RoXcbaF9lIqwpMjiXg54Vw.png"
+                height="400"
+            >
+                <h1 class="text-center">
+                    Scroll too see the parallax effect!
+                </h1>
+            </v-parallax>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
+#bg {
+    max-width: 100%;
+    max-height: 200;
+    background: url(https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/dogs_1280p_0.jpg?itok=cnRk0HYq);
+}
 </style>
