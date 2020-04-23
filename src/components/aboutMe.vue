e138a63fe03b2cdacd54d7460e946457c6dbcd0c
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-04-23 16:40:55 -0400

    deploy

--- src/components/aboutMe.vue
+++ src/components/aboutMe.vue
@@ -7,6 +7,10 @@
         <h1>My bad</h1>
         <h1>I promise</h1>
         <h1>It will look nice</h1>
-        <h1>It will look nice</h1>
+        <h1>Right now I am just typiung some stuff for my video</h1>
+        <h1>Hopefully iy will look like I am coding :D</h1>
+        <h1>yep you gussed it still faking that I am working</h1>
+        <h1>But sectrely, I am just making it looked like I am actually coding :DDDD</h1>
+        <h1>Hye but you have to admit, the parallax effect is pretty nice :D</h1>
     </div>
 </template>
