59b009d1f6bb15ecf08f9eea932d4ac7ab6c5cc0
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-04-22 23:19:36 -0400

    Renamed tabs

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,32 @@
     <v-app id="inspire">
         <v-app-bar app color="white">
             <v-tabs color="rgb(233, 171, 0)" centered show-arrows>
-                <v-tab>Item One</v-tab>
-                <v-tab>Item Two</v-tab>
-                <v-tab>Item Three</v-tab>
+                <v-tab v-for="tabName in tabs" :key="tabName">
+                    {{ tabName }}
+                </v-tab>
             </v-tabs>
         </v-app-bar>
 
         <v-content>
-            <intro></intro>
-            <about-me></about-me>
+            <home id="home"></home>
+            <about-me id="about-me"></about-me>
         </v-content>
     </v-app>
 </template>
 
 <script>
-import intro from "./components/intro"
-import aboutMe from "./components/aboutMe"
-
+import home from "./components/home";
+import aboutMe from "./components/aboutMe";
 
 export default {
+    data() {
+        return {
+            tabs: ["Home", "About Me", "About Me", "About Me", "About Me", "About Me", "About Me"],
+        };
+    },
     components: {
-        intro,
-        "about-me": aboutMe
-    }
+        home,
+        "about-me": aboutMe,
+    },
 };
 </script>
