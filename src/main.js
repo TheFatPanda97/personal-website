a4344330a818e2a0edc39f68df198715f2127b65
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-05-09 20:23:36 -0400

    added google analytics

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-165919387-1'
+});
 
 new Vue({
   vuetify,
