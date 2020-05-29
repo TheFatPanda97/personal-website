78e4e6a751f54c3a2892e76fbfcc9d2e29f79de9
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-05-29 10:56:30 -0400

    updated contact me

--- src/View.vue
+++ src/View.vue
@@ -1,64 +1,41 @@
 <template>
     <v-app>
         <v-app-bar v-if="!dark" color="white" app hide-on-scroll>
-            <v-tabs
-                color="rgb(233, 171, 0)"
-                centered
-                show-arrows
-                v-model="selectedTab"
-            >
-                <v-tab
-                    v-for="tab in tabs"
-                    :key="tab.id"
-                    @click="scrollTo(tab.id)"
-                >
+            <v-tabs color="rgb(233, 171, 0)" centered show-arrows v-model="selectedTab">
+                <v-tab v-for="tab in tabs" :key="tab.id" @click="scrollTo(tab.id)">
                     {{ tab.name }}
                 </v-tab>
             </v-tabs>
         </v-app-bar>
 
         <v-app-bar v-else app color="rgb(31,31,31)" hide-on-scroll dark>
-            <v-tabs
-                color="rgb(233, 171, 0)"
-                centered
-                show-arrows
-                v-model="selectedTab"
-            >
-                <v-tab
-                    v-for="tab in tabs"
-                    :key="tab.id"
-                    @click="scrollTo(tab.id)"
-                >
+            <v-tabs color="rgb(233, 171, 0)" centered show-arrows v-model="selectedTab">
+                <v-tab v-for="tab in tabs" :key="tab.id" @click="scrollTo(tab.id)">
                     {{ tab.name }}
                 </v-tab>
             </v-tabs>
         </v-app-bar>
 
-            <v-responsive>
-                <div id="home">
-                    <div v-intersect.quiet="onIntersect"></div>
-                    <home-view :viewWorkFn="scrollTo" :dark="dark"></home-view>
-                </div>
-                <div id="about-me">
-                    <div v-intersect.quiet="onIntersect"></div>
-                    <about-me-view :dark="dark"></about-me-view>
-                </div>
-                <div id="projects">
-                    <div v-intersect.quiet="onIntersect"></div>
-                    <projects-view :dark="dark"></projects-view>
-                </div>
-                <div id="contact-me">
-                    <contact-me-view :dark="dark"></contact-me-view>
-                </div>
-            </v-responsive>
+        <v-responsive>
+            <div id="home">
+                <div v-intersect.quiet="onIntersect"></div>
+                <home-view :viewWorkFn="scrollTo" :dark="dark"></home-view>
+            </div>
+            <div id="about-me">
+                <div v-intersect.quiet="onIntersect"></div>
+                <about-me-view :dark="dark"></about-me-view>
+            </div>
+            <div id="projects">
+                <div v-intersect.quiet="onIntersect"></div>
+                <projects-view :dark="dark"></projects-view>
+            </div>
+            <div id="contact-me">
+                <contact-me-view :dark="dark"></contact-me-view>
+            </div>
+        </v-responsive>
+        
         <v-footer flat tile padless app>
-            <v-card
-                flat
-                tile
-                width="100%"
-                color="rgb(255, 187, 0)"
-                class="text-center"
-            >
+            <v-card flat tile width="100%" color="rgb(255, 187, 0)" class="text-center">
                 <v-card-text>
                     <v-btn
                         v-for="icon in icons"
@@ -71,12 +48,7 @@
                     >
                         <v-icon large>{{ icon.name }}</v-icon>
                     </v-btn>
-                    <v-btn
-                        icon
-                        class="mx-4"
-                        color="black"
-                        @click="toggleMode()"
-                    >
+                    <v-btn icon class="mx-4" color="black" @click="toggleMode()">
                         <v-icon large>{{
                             !dark ? "mdi-brightness-6" : "mdi-brightness-4"
                         }}</v-icon>
