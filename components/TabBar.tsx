b2f75f707862bb5fad6f5b54a06a6b4b13d07b45
Shawn Hu <shawnhu97@gmail.com>
2022-01-30 13:59:41 -0500

    adjust tab bar styling

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -1,29 +1,37 @@
-const Tab = (): JSX.Element => {
-  return <div className="tab">tab</div>;
+import Image from 'next/image';
+import classNames from 'classnames';
+
+interface ITab {
+  focused: boolean;
+  iconUrl: string;
+  title: string;
+  onClick: () => void;
+}
+
+const Tab = ({ focused, title, onClick, iconUrl }: ITab): JSX.Element => {
+  return (
+    <div
+      className={classNames('tab', {
+        tab__focused: focused,
+      })}
+      onClick={onClick}
+    >
+      <Image src={iconUrl} alt="viaplanner" width={20} height={20} />
+      <div className="w-3" />
+      <p>{title}</p>
+    </div>
+  );
 };
 
 const TabBar = (): JSX.Element => {
   return (
     <div className="tab-bar">
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
+      <Tab
+        focused
+        title="viaplanner"
+        iconUrl="https://raw.githubusercontent.com/VIAplanner/via-timetable/master/public/favicon.ico"
+        onClick={() => {}}
+      />
     </div>
   );
 };
