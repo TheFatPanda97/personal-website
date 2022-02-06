9b02d98c1eef4925c53c4c682bc43990e646626d
Shawn Hu <shawnhu97@gmail.com>
2022-02-06 18:52:15 -0500

    finished lesson 4

--- lesson4-webpack/src/script.js
+++ lesson4-webpack/src/script.js
@@ -1,1 +1,31 @@
-import './style.css'
+import './style.css';
+import * as THREE from 'three';
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 'red' });
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+// Camera
+const sizes = {
+  width: 800,
+  height: 600,
+};
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.y = 1;
+camera.position.x = 1;
+
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+console.log(canvas);
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
