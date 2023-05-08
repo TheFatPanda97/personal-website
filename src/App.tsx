a3385c10535936d1de5d398f15e9c3e0585643eb
TheFatPanda97 <shawnhu97@gmail.com>
2023-05-08 18:44:17 -0400

    added animated text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,82 @@
+import './index.scss';
+import useScrollRerender from './hooks/useScrollRerender';
+
 import AnimatedText from './components/AnimatedText';
 
 const App = () => {
+  useScrollRerender();
+
   return (
     <div>
-      Hello There
-      <AnimatedText width={20}>This is a test</AnimatedText>
+      <div style={{ height: 1000 }}></div>
+      <AnimatedText speed={3}>This is a test</AnimatedText>
+      <AnimatedText>This is another test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <AnimatedText>This is a test</AnimatedText>
+      <div style={{ height: 1000 }}></div>
     </div>
   );
 };
--- src/components/AnimatedText/index.tsx
+++ src/components/AnimatedText/index.tsx
@@ -1,8 +1,9 @@
-import type { FC, ReactNode } from 'react';
+import { useState } from 'react';
 import './style.scss';
 
+import type { FC, ReactNode } from 'react';
 interface IProps {
-  width: number;
+  speed?: number;
   children: ReactNode;
   fontSize?: number;
   unfillColor?: string;
@@ -10,19 +11,33 @@ interface IProps {
 }
 
 const AnimatedText: FC<IProps> = ({
-  width,
+  speed = 1,
   children,
   fontSize = 24,
   unfillColor = 'grey',
   fillColor = '#88c5cc',
 }) => {
+  const [fillWidth, setFillWidth] = useState(0);
+
   return (
-    <div className="animated-text-container">
+    <div
+      className="animated-text-container"
+      ref={(el) => {
+        if (!el) return;
+
+        const currTop = el.getBoundingClientRect().top >= 0 ? el.getBoundingClientRect().top : 0;
+
+        if (currTop <= window.innerHeight) {
+          const val = (100 - (currTop / window.innerHeight) * 100) * speed;
+          setFillWidth(val < 100 ? val : 100);
+        }
+      }}
+    >
       <div className="inner-container">
         <p className="background-text" style={{ fontSize, color: unfillColor }}>
           {children}
         </p>
-        <p className="fill-text" style={{ fontSize, color: fillColor, width: `${width}%` }}>
+        <p className="fill-text" style={{ fontSize, color: fillColor, width: `${fillWidth}%` }}>
           {children}
         </p>
       </div>
