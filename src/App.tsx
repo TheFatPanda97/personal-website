62195ff31085415d8d4e883d1159be9c6a227fa3
TheFatPanda97 <shawnhu97@gmail.com>
2023-05-08 22:38:30 -0400

    changed background color to black

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import useScrollRerender from './hooks/useScrollRerender';
 
 import AnimatedText from './components/AnimatedText';
 import Tabs from './components/Tabs';
+import Home from './sections/HomeSection';
 
 const App = () => {
   useScrollRerender();
@@ -12,7 +13,7 @@ const App = () => {
       <div className="top-vignette" />
       <img src="./logo.png" alt="logo" width={35} className="logo-img" />
       <Tabs />
-      <div style={{ height: 1000 }}></div>
+      <Home />
       <AnimatedText speed={3}>This is a test</AnimatedText>
       <AnimatedText>This is another test</AnimatedText>
       <AnimatedText>This is a test</AnimatedText>
---
+++ src/sections/HomeSection/index.tsx
@@ -0,0 +1,14 @@
+import './style.scss';
+
+const Home = () => {
+  return (
+    <div className="home-section">
+      <h2 className="name">Shawn Hu</h2>
+      <h1>SOFTWARE ENGINEER</h1>
+      <h1 className="special">DREAMER</h1>
+      <h1>PROBLEM SOLVER</h1>
+    </div>
+  );
+};
+
+export default Home;
