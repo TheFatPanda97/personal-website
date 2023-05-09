d6d31de02440028ce2241dbc84d0429f03826cd2
TheFatPanda97 <shawnhu97@gmail.com>
2023-05-08 22:43:06 -0400

    added parallax

--- src/sections/HomeSection/index.tsx
+++ src/sections/HomeSection/index.tsx
@@ -1,13 +1,17 @@
 import './style.scss';
 
+import { Parallax } from 'react-parallax';
+
 const Home = () => {
   return (
-    <div className="home-section">
-      <h2 className="name">Shawn Hu</h2>
-      <h1>SOFTWARE ENGINEER</h1>
-      <h1 className="special">DREAMER</h1>
-      <h1>PROBLEM SOLVER</h1>
-    </div>
+    <Parallax bgImage="./intro-background-dark.png" strength={200}>
+      <div className="home-section">
+        <h2 className="name">Shawn Hu</h2>
+        <h1>SOFTWARE ENGINEER</h1>
+        <h1 className="special">DREAMER</h1>
+        <h1>PROBLEM SOLVER</h1>
+      </div>
+    </Parallax>
   );
 };
 
