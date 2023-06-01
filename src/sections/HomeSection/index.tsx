1d2bc5b5955375e1e1fdc2f04a1d2644d79cabaf
TheFatPanda97 <shawnhu97@gmail.com>
2023-06-01 12:41:24 -0400

    finished home section

--- src/sections/HomeSection/index.tsx
+++ src/sections/HomeSection/index.tsx
@@ -1,17 +1,24 @@
+/// <reference types="vite-plugin-svgr/client" />
 import './style.scss';
-
-import { Parallax } from 'react-parallax';
+import { ReactComponent as HeartBeat } from '../../assets/heartbeat.svg';
+import { ReactComponent as LighBulb } from '../../assets/lightbulb.svg';
+import { ReactComponent as Dream } from '../../assets/dream.svg';
+import { ReactComponent as Wrench } from '../../assets/wrench.svg';
 
 const Home = () => {
   return (
-    <Parallax bgImage="./intro-background-dark.png" strength={200}>
-      <div className="home-section">
+    <div className="home-section">
+      {/* <HeartBeat className="heartbeat" /> */}
+      <LighBulb className="lightbulb" />
+      <Dream className="dream" />
+      <Wrench className="wrench" />
+      <div className="text-container">
         <h2 className="name">Shawn Hu</h2>
         <h1>SOFTWARE ENGINEER</h1>
         <h1 className="special">DREAMER</h1>
         <h1>PROBLEM SOLVER</h1>
       </div>
-    </Parallax>
+    </div>
   );
 };
 
